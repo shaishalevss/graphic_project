63623bd2067ddae619008f12ca0fb135b53a97ad
shaishalevss <22shaiss@gmail.com>
2020-10-27 18:23:43 +0200

    added test file for Coordinate. and fixed the coordinate get and set methods names.

---
+++ src/Unittests/CoordinateTest.java
@@ -0,0 +1,27 @@
+package Unittests;
+import Primitives.Coordinate;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordinateTest {
+
+    @Test
+    void emptyCoordinate(){
+        Coordinate coor = new Coordinate();
+        assertEquals(0.0, coor.getCoordinate());
+    }
+
+    @Test
+    void setCoordinate(){
+        Coordinate coor = new Coordinate();
+        coor.setCoordinate(5.0);
+        assertEquals(5.0,coor.getCoordinate());
+    }
+
+    @Test
+    void equalsMethod(){
+        Coordinate coor1 = new Coordinate(2.0);
+        Coordinate coor2 = new Coordinate(3.0);
+        assertEquals(false, coor1.equals(coor2));
+    }
+}
