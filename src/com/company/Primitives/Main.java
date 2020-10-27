4cd9fed0eeece315eb593cf55eef66039108b41a
Ron <bronitransporter@gmail.com>
2020-10-27 14:29:17 +0200

    Added Coordinate Class

---
+++ src/com/company/Primitives/Main.java
@@ -0,0 +1,12 @@
+package com.company.Primitives;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Coordinate d = new Coordinate();
+
+        System.out.println(d);
+    }
+}
