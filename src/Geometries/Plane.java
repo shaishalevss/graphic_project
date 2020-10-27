c6000921f7ba8af3b6dad3d22f9befde418efbaf
shaishalevss <22shaiss@gmail.com>
2020-10-27 15:35:09 +0200

    edited Plane, Polygon.

--- src/Geometries/Plane.java
+++ src/Geometries/Plane.java
@@ -1,4 +1,49 @@
 package Geometries;
+import Primitives.Point3D;
+import Primitives.Vector;
 
 public class Plane extends AbstractGeometry{
+    protected Point3D _q;
+    protected Vector _n;
+
+    //constructors
+    public Plane(Point3D _q, Vector _n) {
+        this._q = _q;
+        this._n = _n;
+    }
+
+    public Plane(){
+        this._q = new Point3D();
+        this._n = new Vector();
+    }
+
+    public Plane(Plane otherPlane){
+        this._q = otherPlane.get_q();
+        this._n = otherPlane.get_n();
+    }
+
+    //getters
+    public Point3D get_q() {
+        return _q;
+    }
+
+    public void set_q(Point3D _q) {
+        this._q = _q;
+    }
+
+    //setters
+    public Vector get_n() {
+        return _n;
+    }
+
+    public void set_n(Vector _n) {
+        this._n = _n;
+    }
+
+    //equals methods
+    @Override
+    public boolean equals(Object otherPlane) {
+        return (this._q==((Plane)otherPlane).get_q()&&this._n==((Plane)otherPlane).get_n());
+    }
+
 }
--- src/Geometries/Polygon.java
+++ src/Geometries/Polygon.java
@@ -1,4 +1,37 @@
 package Geometries;
+import Primitives.Point3D;
+import java.util.List;
 
 public class Polygon extends AbstractGeometry{
+    protected List<Point3D> _points;
+
+    //constructors
+    public Polygon(List<Point3D> _points) {
+        this._points = _points;
+    }
+
+    public Polygon(){
+        this._points = null;
+    }
+
+    public Polygon(Polygon otherPolygon){
+        this._points = otherPolygon.get_points();
+
+    }
+
+    //getters
+    public List<Point3D> get_points() {
+        return _points;
+    }
+
+    //setters
+    public void set_points(List<Point3D> _points) {
+        this._points = _points;
+    }
+
+    //equal method override
+    @Override
+    public boolean equals(Object otherPolygon) {
+        return (this._points==((Polygon)otherPolygon).get_points());
+    }
 }
