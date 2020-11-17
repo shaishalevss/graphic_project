9ebad9f5d22edc699f17a260ebcd57c1669f9295
Ron <bronitransporter@gmail.com>
2020-11-17 19:24:54 +0200

    Created Camera Class

--- src/Elements/Camera.java
+++ src/Elements/Camera.java
@@ -9,6 +9,7 @@ public class Camera {
     protected Vector _vRight;
     protected Vector _vToward;
 
+    //Default Constructor.
     public Camera() {
         this._projectionCenter = new Point3D();
         this._vUp = new Vector();
@@ -16,10 +17,53 @@ public class Camera {
         this._vToward = new Vector();
     }
 
+    //Basic Constructor
     public Camera(Vector vUp, Vector vRight) {
         this._projectionCenter = new Point3D();
         this._vUp = new Vector(vUp);
         this._vRight = new Vector(vRight);
         this._vToward = new Vector(vUp.crossProduct(vRight));
+    }
+
+    //Copy Constructor
+    public Camera(Camera otherCamera) {
+        this._projectionCenter = otherCamera.getProjectionCenter();
+        this._vUp = otherCamera.getVUp();
+        this._vRight = otherCamera.getVRight();
+        this._vToward = otherCamera.getVToward();
+    }
+
+    //Getters
+    public Point3D getProjectionCenter() {
+        return new Point3D(this._projectionCenter);
+    }
+
+    public Vector getVUp() {
+        return new Vector(this._vUp);
+    }
+
+    public Vector getVRight() {
+        return new Vector(this._vRight);
+    }
+
+    public Vector getVToward() {
+        return new Vector(this._vToward);
+    }
+
+    //Setters
+    public void setProjectionCenter(Point3D newCenter) {
+        this._projectionCenter = new Point3D(newCenter);
+    }
+
+    public void setVUp(Vector newVUp) {
+        this._vUp = new Vector(newVUp);
+    }
+
+    public void setVRight(Vector newVRight) {
+        this._vRight = new Vector(newVRight);
+    }
+
+    public void setVToward(Vector newVToward) {
+        this._vToward = new Vector(newVToward);
     }
 }
