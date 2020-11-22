9011e6584c16f8f0177dd34dbc33e6139aaef8f2
Ron <bronitransporter@gmail.com>
2020-11-22 22:38:58 +0200

    Added Camera class and test

--- src/Elements/Camera.java
+++ src/Elements/Camera.java
@@ -4,6 +4,9 @@ import Primitives.Point3D;
 import Primitives.Ray;
 import Primitives.Vector;
 
+import java.awt.*;
+import java.util.Objects;
+
 public class Camera {
     protected Point3D _projectionCenter;
     protected Vector _vUp;
@@ -19,11 +22,11 @@ public class Camera {
     }
 
     //Basic Constructor
-    public Camera(Vector vUp, Vector vRight) {
+    public Camera(Vector vToward, Vector vUp) {
         this._projectionCenter = new Point3D();
+        this._vToward = new Vector(vToward);
         this._vUp = new Vector(vUp);
-        this._vRight = new Vector(vRight);
-        this._vToward = new Vector(vUp.crossProduct(vRight));
+        this._vRight = new Vector(vToward.crossProduct(vUp));
     }
 
     //Copy Constructor
@@ -68,8 +71,35 @@ public class Camera {
         this._vToward = new Vector(newVToward);
     }
 
-    public Ray constructRayThrowAPixel(int nX, int nY, int j, int i, double screenDistance, double screenWidth, double screenHeight) {
 
-        return new Ray();
+    public Ray constructRayThroughPixel(int nX, int nY, int j, int i, double screenDistance, double screenWidth, double screenHeight) {
+        Point3D pIJ = this._projectionCenter.add(this._vToward.scale(screenDistance));  //Center point of the plane
+        double rX = screenWidth/nX;                                                     //Calculating the size
+        double rY = screenHeight/nY;                                                    //of the pixels on the plane
+
+        double yI = rY*(i-((double)(nY-1)/2));                                          //Movement on the y axis
+        double xJ = rX*(j-((double)(nX-1)/2));                                          //Movement on the x axis
+
+        if (xJ != 0)                                                                    //If there is movement on the x
+            pIJ = pIJ.add(_vRight.scale(xJ));                                           //axis then move
+        if (yI != 0)                                                                    //If there is movement on the y
+            pIJ = pIJ.add(_vUp.scale(-yI));                                             //axis then move
+
+        Vector vIJ = new Vector(pIJ.subtract(_projectionCenter));                       //cast a vector from camera to desired pixel
+
+        return new Ray(new Point3D(_projectionCenter),vIJ.normalize());
+    }
+
+    @Override
+    public String toString() {
+        return "" + _projectionCenter + " " + _vUp + " " + _vRight + " " + _vToward;
+    }
+
+    @Override
+    public boolean equals(Object otherCamera) {
+        return this._projectionCenter.equals(((Camera)otherCamera)._projectionCenter)&&
+                this._vToward.equals(((Camera)otherCamera)._vToward)&&
+                this._vUp.equals(((Camera)otherCamera)._vUp)&&
+                this._vRight.equals(((Camera)otherCamera)._vRight);
     }
 }
