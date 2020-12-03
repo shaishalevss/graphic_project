9609861ec4bcc2d37645547a23b93dd3bfe4d8ec
shaishalevss <22shaiss@gmail.com>
2020-12-03 23:00:07 +0200

    Added some extra tests, and fixed the test file name.

--- src/Unittests/ImageWriterTest.java
+++ src/Unittests/ImageWriterTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import Renderer.ImageWriter;
 
-class testImageWriter {
+class ImageWriterTest {
 
     @Test
     public void test() {
@@ -19,6 +19,34 @@ class testImageWriter {
             for (int j = 0; j < nX; ++j)
                 imageWriter.writePixel(j, i,
                         j % 50 == 0 || i % 50 == 0 ? new Color(255,0,0) : new Color(255,255,0));
+
+        imageWriter.writeToImage();
+    }
+
+    @Test
+    public void test2() {
+        ImageWriter imageWriter = new ImageWriter("anotherImageTest", 1600, 1000, 800, 500);
+        int nX = imageWriter.getNx();
+        int nY = imageWriter.getNy();
+
+        for (int i = 0; i < nY; ++i)
+            for (int j = 0; j < nX; ++j)
+                imageWriter.writePixel(j, i,
+                        j % 50 == 0  ? new Color(0,204,204) : new Color(102,204,0));
+
+        imageWriter.writeToImage();
+    }
+
+    @Test
+    public void test3() {
+        ImageWriter imageWriter = new ImageWriter("anotherTwoImageTest", 1600, 1000, 800, 500);
+        int nX = imageWriter.getNx();
+        int nY = imageWriter.getNy();
+
+        for (int i = 0; i < nY; ++i)
+            for (int j = 0; j < nX; ++j)
+                imageWriter.writePixel(j, i,
+                        j % 50 != 0  ? new Color(0,204,204) : new Color(102,204,0));
 
         imageWriter.writeToImage();
     }
